d180184b637f8211a6204df24a08393c76e041ed
Ghislain Lévêque <ghislain.leveque@gmail.com>
2021-11-03 21:11:22 +0100

    v11

--- 1/index.js
+++ 1/index.js
@@ -1,5 +1,12 @@
 const text = document.getElementById("debug");
-const VERSION = "6";
+const camera = document.getElementById("camera");
+const VERSION = "11";
+const velocity = {
+    forward: 0,
+    lateral: 0,
+    vertical: 0
+};
+
 text.setAttribute("value", "Loading.... " + VERSION);
 
 const doLog = (message) => {
@@ -13,17 +20,42 @@ AFRAME.registerComponent('thumbstick-logging', {
         this.el.addEventListener("triggerdown", this.triggerPressed);
     },
     logThumbstick: function (evt) {
-        if (evt.detail.y > 0.95) { text.setAttribute("value","DOWN"); }
-        if (evt.detail.y < -0.95) { text.setAttribute("value","UP"); }
-        if (evt.detail.x < -0.95) { text.setAttribute("value","LEFT"); }
-        if (evt.detail.x > 0.95) { text.setAttribute("value","RIGHT"); }
+        if (evt.detail.y > 0.95) {
+            velocity.forward += evt.detail.y;
+            doLog("DOWN " + velocity.forward.toString());
+        }
+        if (evt.detail.y < -0.95) {
+            velocity.forward += evt.detail.y;
+            doLog("UP " + velocity.forward.toString());
+        }
+        if (evt.detail.x < -0.95) {
+            velocity.lateral += evt.detail.y;
+            doLog("LEFT " + velocity.lateral.toString());
+        }
+        if (evt.detail.x > 0.95) {
+            velocity.lateral += evt.detail.y;
+            doLog("RIGHT " + velocity.lateral.toString());
+        }
     },
     triggerPressed: function (evt) {
-        text.setAttribute("value", "Trigger");
+        doLog( "Trigger");
+        velocity.forward = 0;
+        velocity.lateral = 0;
+        velocity.vertical = 0;
     }
 });
 
 setTimeout(() => {
     doLog("Ready!" + VERSION);
 }, 1000);
+
+
+setTimeout(() => {
+    doLog("Still there!" + VERSION);
+}, 10000);
+
+
+setTimeout(() => {
+    doLog("Not dead!" + VERSION);
+}, 30000);
 
