3409c27bfed2ca78f4b4321923c8f518b097fde2
Ghislain Lévêque <ghislain.leveque@gmail.com>
2021-11-03 20:51:02 +0100

    button detection works

--- 1/index.js
+++ 1/index.js
@@ -1,10 +1,6 @@
 const text = document.getElementById("debug");
-const velocity = {
-    forward: 0,
-    lateral: 0,
-    vertical: 0
-};
-text.setAttribute("value", "Loading....");
+const VERSION = "4";
+text.setAttribute("value", "Loading.... " + VERSION);
 
 const doLog = (message) => {
     console.log(message);
@@ -17,54 +13,17 @@ AFRAME.registerComponent('thumbstick-logging', {
         this.el.addEventListener("triggerdown", this.triggerPressed);
     },
     logThumbstick: function (evt) {
-        if (evt.detail.y > 0.95) { doLog("DOWN"); }
-        if (evt.detail.y < -0.95) { doLog("UP"); }
-        if (evt.detail.x < -0.95) { doLog("LEFT"); }
-        if (evt.detail.x > 0.95) { doLog("RIGHT"); }
+        if (evt.detail.y > 0.95) { text.setAttribute("value","DOWN"); }
+        if (evt.detail.y < -0.95) { text.setAttribute("value","UP"); }
+        if (evt.detail.x < -0.95) { text.setAttribute("value","LEFT"); }
+        if (evt.detail.x > 0.95) { text.setAttribute("value","RIGHT"); }
     },
     triggerPressed: function (evt) {
-        doLog("trigger");
+        text.setAttribute("value", "Trigger");
     }
 });
-/*
-AFRAME.registerComponent('thumbstick-move', {
-    init: function() {
-        this.el.addEventListener('thumbstickmoved', this.thumbstickMoved);
-        doLog(JSON.stringify(velocity));
-    },
-    thumbstickMoved: function(evt) {
-        if (evt.detail.y > 0.95) {
-            velocity.forward -= 1;
-            doLog(JSON.stringify(velocity));
-        }
-        if (evt.detail.y < -0.95) {
-            velocity.forward += 1;
-            doLog(JSON.stringify(velocity));
-        }
-        if (evt.detail.x < -0.95) {
-            velocity.lateral -= 1;
-            doLog(JSON.stringify(velocity));
-        }
-        if (evt.detail.x > 0.95) {
-            velocity.lateral += 1;
-            doLog(JSON.stringify(velocity));
-        }
-    }
-})
-*/
 
 setTimeout(() => {
-    doLog("Ready!");
+    doLog("Ready!" + VERSION);
 }, 1000);
 
-setTimeout(() => {
-    doLog("Still there!");
-}, 10000);
-
-setTimeout(() => {
-    doLog("Not dead!");
-}, 30000);
-
-/*
-      <a-entity oculus-touch-controls="hand: left" thumbstick-move></a-entity>
-*/
